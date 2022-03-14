9763b322618f7cdae84fc7c0d49c4d3be3c79924
Tim <juitingtsai0808@gmail.com>
2022-03-14 14:18:35 +0800

    create model.py for Creates User info

---
+++ app/app/test/test_model.py
@@ -0,0 +1,25 @@
+import email
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+class ModelTest(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating a new user with and email is successful"""
+        email = "test@gmail.com"
+        password = "testpass123"
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password
+        )
+
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
+    
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = "testABC@gmail.com"
+        user = get_user_model().objects.create_user(
+            email , 'test123'
+        )
+        self.assertEqual(user.email,email.lower())
