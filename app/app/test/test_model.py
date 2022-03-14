827877721898081f74b479552272020c30edd887
Tim <juitingtsai0808@gmail.com>
2022-03-14 14:59:26 +0800

    create a superuser

--- app/app/test/test_model.py
+++ app/app/test/test_model.py
@@ -1,4 +1,3 @@
-import email
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -26,3 +25,11 @@ class ModelTest(TestCase):
         """Test creating user with no email raises error """
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user(None,'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user=get_user_model().objects.create_superuser(
+            "testABC@gmail.com","test123"
+        )
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
--- app/core/models.py
+++ app/core/models.py
@@ -12,6 +12,14 @@ class UserManager(BaseUserManager):
         user.save( using = self._db)
 
         return user
+    def create_superuser(self,email,password):
+        """Create and saves a new super user"""
+        user = self.create_user(email,password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+
+        return user
 class User(AbstractBaseUser,PermissionsMixin): 
     """Custom user model that supports using email instead of username """
     email = models.EmailField(max_length=255,unique=True)
